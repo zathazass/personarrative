648b263769134dc4cdf67dfd3b3eeb7e43d2f85b
zathazass <sathananthanit15@gmail.com>
2024-02-24 00:00:06 +0530

    strict mode for early bug finding in development

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { StrictMode } from 'react';
 
 const domNode = document.getElementById('root') as HTMLElement;
 const root = createRoot(domNode);
 
-root.render(<App />)
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
