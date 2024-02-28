e7511a0b5b6fcb8bf215fbf0326d124278df8317
zathazass <sathananthanit15@gmail.com>
2024-02-28 21:57:06 +0530

    tailwind setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-    return <h1>My first React app with Typescript!</h1>
+    return (
+    <>
+        <div className="bg-slate-200 border-2 border-slate-400 p-4 w-[70vw] mx-auto my-8">
+            <h1 className="font-medium text-xl text-teal-600 mb-3">My first React app with Typescript!</h1>
+            <p className="leading-relaxed text-teal-900">It is quite difficult for me as a backend developer, but yeah , I like it. It feels good to setup one by one and also engaging.</p>
+        </div>
+    </>)
+
 }
